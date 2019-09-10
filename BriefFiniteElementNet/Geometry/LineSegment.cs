b4d94f6f08ffe8becffb144d4820a7c91e65d6ba
epsi1on <ehsan.ma@gmx.com>
2019-09-10 06:18:30 +0430

    update

--- BriefFiniteElementNet/Geometry/LineSegment.cs
+++ BriefFiniteElementNet/Geometry/LineSegment.cs
@@ -11,8 +11,8 @@ namespace BriefFiniteElementNet.Geometry
 
         public LineSegment(Point p1, Point p2)
         {
-            p1 = p1;
-            p2 = p2;
+            this.p1 = p1;
+            this.p2 = p2;
         }
 
        
