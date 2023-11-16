db7a7f57144bfcedbb694c8aced0a74ad6a2f414
epsi1on <ehsan.ma@gmx.com>
2023-11-16 22:15:49 +0330

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ release = '2.1.1'
 source_suffix = {'.rst': 'restructuredtext'}
 root_doc = 'index'
 master_doc = 'index'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 #extensions = ['rst2pdf.pdfbuilder']
 #df_documents = [('index', u'rst2pdf', u'BriefFiniteElement documentation', u'epsi1on'),]
